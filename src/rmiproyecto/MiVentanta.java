576a042d30cfa11ecfd54626c0478f0611744385
Martin Pastora <muko09@unitec.edu>
2013-12-09 21:14:29 -0600

    nada

--- src/rmiproyecto/MiVentanta.java
+++ src/rmiproyecto/MiVentanta.java
@@ -603,8 +603,7 @@ public class MiVentanta extends javax.swing.JFrame   {
     }//GEN-LAST:event_conversationsMouseClicked
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-        //MARTIN AQUÍ VA TU CÓDIGO
+        
         
     }//GEN-LAST:event_jButton3ActionPerformed
 
